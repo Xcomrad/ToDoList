50b168a5c4f6a34aa1dbbd25e912eb26a497a1dd
Xcomrad <butgus.dallas@gmail.com>
2024-06-24 18:09:29 +0300

    initialize project

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Владислав Бут-Гусаим on 24.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

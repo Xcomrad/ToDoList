0b9e4e682af60318d3177b2a9fe128e0e7a90eb7
Xcomrad <butgus.dallas@gmail.com>
2024-06-25 12:27:23 +0300

    refact navBar, tableView, dataManager

--- ToDoList/ToDoList/Controller/MainController.swift
+++ ToDoList/ToDoList/Controller/MainController.swift
@@ -2,13 +2,9 @@
 import UIKit
 import SnapKit
 
-class MainController: UIViewController {
-    
-    var items: [Model] = []
+final class MainController: UIViewController {
     
     private let mainView = MainView()
-    private let dataManager = DataManager()
-    private let itemsManager = ItemsManager()
     
     override func loadView() {
         view = mainView
@@ -16,12 +12,12 @@ class MainController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navBarSettings()
+        setupNavItems()
     }
     
     override func viewIsAppearing(_ animated: Bool) {
         super.viewIsAppearing(animated)
-        items = itemsManager.toDoItems
+        mainView.dataManager.items = mainView.dataManager.getItem()
     }
 }
 
@@ -29,33 +25,45 @@ class MainController: UIViewController {
 
 extension MainController {
     
-    func navBarSettings() {
-        title = "To-Do Items"
-       
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewTask))
-        navigationItem.rightBarButtonItem = addButton
+    func setupNavItems() {
+        title = "To-Do List"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewTask))
+        addButton.tintColor = .white
+        
+//        let editButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editTasks))
+//        editButton.tintColor = .white
+        
+        navigationItem.rightBarButtonItem = addButton
+        //navigationItem.leftBarButtonItem = editButton
     }
     
-   @objc func addNewTask() {
-       let alertController = UIAlertController(title: "Новая Заметка",
-                                               message: "Введите название заметки",
-                                               preferredStyle: .alert)
-               alertController.addTextField()
-               
-               let addAction = UIAlertAction(title: "Добавить", style: .default) { _ in
-                   if let title = alertController.textFields?.first?.text, !title.isEmpty {
-                       let newItem = Model(id: UUID(), title: title, isCompleted: false)
-                       self.items.append(newItem)
-                       self.mainView.tableView.insertRows(at: [IndexPath(row: self.items.count - 1, section: 0)], with: .automatic)
-                       self.dataManager.saveItems([newItem])
-                   }
-               }
-               let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
-               
-               alertController.addAction(addAction)
-               alertController.addAction(cancelAction)
-               
-               present(alertController, animated: true, completion: nil)
+    @objc func addNewTask() {
+        let alertController = UIAlertController(title: "Новая Заметка",
+                                                message: "Введите название заметки",
+                                                preferredStyle: .alert)
+        alertController.addTextField()
+        
+        let addAction = UIAlertAction(title: "Добавить", style: .default) { _ in
+            if let title = alertController.textFields?.first?.text, !title.isEmpty {
+                
+                let newItem = Model(id: UUID(), title: title, isCompleted: false)
+                self.mainView.dataManager.items.append(newItem)
+                self.mainView.tableView.insertRows(at: [IndexPath(row: self.mainView.dataManager.items.count - 1,
+                                                                  section: 0)], with: .automatic)
+                self.mainView.dataManager.saveItem()
+                
+                UIView.animate(withDuration: 0.3) {
+                    self.mainView.tableView.layoutIfNeeded()
+                }
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
     }
 }
+
--- ToDoList/ToDoList/View/TableViewCell.swift
+++ ToDoList/ToDoList/View/TableViewCell.swift
@@ -1,7 +1,14 @@
 
 import UIKit
 
+protocol TableViewCellDelegate: AnyObject {
+    func didToggleComplete(for item: Model)
+}
+
 class TableViewCell: UITableViewCell {
+    
+    weak var delegate: TableViewCellDelegate?
+    private var item: Model?
     
     static let reuseId = "TableViewCell"
     
@@ -10,6 +17,12 @@ class TableViewCell: UITableViewCell {
         label.font = UIFont.boldSystemFont(ofSize: 18)
         label.textAlignment = .left
         return label
+    }()
+    
+    private lazy var completeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.addTarget(self, action: #selector(completeButtonTapped), for: .touchUpInside)
+        return button
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -22,9 +35,29 @@ class TableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // Update
+
+    //MARK: - Action
+    @objc private func completeButtonTapped() {
+        guard let item = item else { return }
+        delegate?.didToggleComplete(for: item)
+    }
+    
+    //MARK: - Update
     func update(_ item: Model) {
+        self.item = item
         nameItemLabel.text = item.title
+        
+        let titleColor: UIColor = item.isCompleted ? .lightGray : .black
+        nameItemLabel.textColor = titleColor
+        
+        let buttonTitle = item.isCompleted ? "Завершено" : "В процессе"
+        completeButton.setTitle(buttonTitle, for: .normal)
+        
+        let buttonTitleColor: UIColor = item.isCompleted ? .systemGreen : .systemRed
+        completeButton.setTitleColor(buttonTitleColor, for: .normal)
+        
+        let backGroundCollor: UIColor = item.isCompleted ? .systemGray6 : .white
+        contentView.backgroundColor = backGroundCollor
     }
 }
 
@@ -34,12 +67,18 @@ extension TableViewCell {
     
     func setupViews() {
         contentView.addSubview(nameItemLabel)
+        contentView.addSubview(completeButton)
     }
     
     func setupConstraints() {
         nameItemLabel.snp.makeConstraints { make in
-            make.top.bottom.equalTo(contentView).inset(10)
+            make.top.bottom.equalTo(contentView)
             make.leading.trailing.equalTo(contentView).inset(20)
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.trailing.equalTo(contentView).inset(20)
+            make.centerY.equalTo(contentView)
         }
     }
 }
